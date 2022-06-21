a6934f81d8e9a45edc0d639116e90a5e46893d05
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2022-06-21 10:18:57 +0200

    added switch in results, regrow_saw
    In  consts  added switch to  distinguish between output result folders.
    Also add regrow_saw for  self-avoiding walks.

--- chromosome_segregation/consts.py
+++ chromosome_segregation/consts.py
@@ -2,11 +2,18 @@ caches = None
 # ds  = 0.1
 
 
-size_x = 50
-size_y = 50
-size_z = 50
+size_x = 100
+size_y = 100
+size_z = 100
+
+RUN_FREE = False
 
 volume = (size_y+1) * (size_x+1) * (size_z+1)
 
-RESULTS_FOLDER = 'results_box'+str(size_x) +'_'+ str(size_y) + '_'+ str(size_z)
-# RESULTS_FOLDER = 'results'
+RESULTS_FOLDER_FREE = 'results' #'results_box'+str(size_x) +'_'+ str(size_y) + '_'+ str(size_z)
+RESULTS_FOLDER_HALFSPACE = 'results_halfspace'
+
+if RUN_FREE:
+    RESULTS_FOLDER = RESULTS_FOLDER_FREE
+else:
+    RESULTS_FOLDER = RESULTS_FOLDER_HALFSPACE
--- chromosome_segregation/regrow.py
+++ chromosome_segregation/regrow.py
@@ -2,13 +2,15 @@
 Changing chain conformation
 """
 
-import math
-import  numpy as np
-import  sys
+import sys
+
+import numpy as np
+
 try:
     import consts, aux
 except:
     from chromosome_segregation import consts, aux
+
 
 def regrow(n, dx, dy, dz, res):
     """
@@ -30,9 +32,12 @@ def regrow(n, dx, dy, dz, res):
         for n_ in neighbours:
 
             # checking if outside the box
-            is_inside = aux.is_inside_box(*n_[1:])
+            if consts.RUN_FREE:
+                is_inside = 1
+            else:
+                is_inside = aux.is_inside_box(*n_[1:])
 
-            if is_inside ==1:
+            if is_inside == 1:
                 counts.append(consts.caches[n_[0] - 1, abs(n_[1]), abs(n_[2]), abs(n_[3])])
             else:
                 counts.append(0)
@@ -57,9 +62,7 @@ def regrow(n, dx, dy, dz, res):
         #         print(neighbours[selected])
         res.append(neighbours[selected][1:])
 
-
         return regrow(*neighbours[selected], res)
-
 
 
 def regrow_biased(n, dx, dy, dz, res, w, alpha, k):
@@ -91,135 +94,17 @@ def regrow_biased(n, dx, dy, dz, res, w, alpha, k):
             all_coincidence.append(n_coincide)
 
             # checking if outside the box
-            is_inside = aux.is_inside_box(*neighbour[1:])
-            # if (is_inside ==0): print("OUTSiDE")
-
-
-            #             print(coords, n_coincide)
-            count = consts.caches[neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3])]
-            counts.append(np.exp(-alpha * n_coincide) * count * is_inside)
-            tmp += count* is_inside  # accumulating the denominator
-
-        # calculating W
-        w = w * sum(counts) / tmp
-        #         print(w)
-        # normalising p_i
-        counts = [c / sum(counts) for c in counts]
-
-        # making cumulative sum
-        counts_ = np.cumsum(counts)
-        if sum(counts_) == 0:
-            print('failed to grow')
-            sys.exit()
-        # selecting one of neigbours
-        selected = np.argmax(counts_ > np.random.random())
-        res.append(neighbours[selected][1:])
-        if counts[selected] == 0:
-            print('SELECTED NOT POSSIBLE')
-        k += all_coincidence[selected]
-        return regrow_biased(*neighbours[selected], res, w, alpha, k)
-
-"""
-Changing chain conformation
-"""
-
-import math
-import  numpy as np
-import  sys
-try:
-    import consts, aux
-except:
-    from chromosome_segregation import consts, aux
-
-def regrow(n, dx, dy, dz, res):
-    """
-    recursive uniform regrow.
-    The generated coordinates are uniform random
-    """
-
-    if n == 1:
-        return res + [[0, 0, 0]]
-    else:
-        neighbours = [[n - 1, dx - 1, dy, dz],
-                      [n - 1, dx + 1, dy, dz],
-                      [n - 1, dx, dy - 1, dz],
-                      [n - 1, dx, dy + 1, dz],
-                      [n - 1, dx, dy, dz - 1],
-                      [n - 1, dx, dy, dz + 1]
-                      ]
-        counts = []
-        for n_ in neighbours:
-
-            # checking if outside the box
-            is_inside = aux.is_inside_box(*n_[1:])
-
-            if is_inside ==1:
-                counts.append(consts.caches[n_[0] - 1, abs(n_[1]), abs(n_[2]), abs(n_[3])])
+            if consts.RUN_FREE:
+                is_inside = 1
             else:
-                counts.append(0)
-
-        # normalising
-        counts = [c / sum(counts) for c in counts]
-        # uniform sampling
-        #         nonzeros = [v for v in counts if v>0]
-        #         counts = [1/len(nonzeros) if c !=0 else c for c in counts]
-        # wrong sampling
-        #         counts = [1/6. for i in range(6)]
-
-        #         print(counts)
-
-        # making cumulative sum
-        counts_ = np.cumsum(counts)
-        if sum(counts_) == 0:
-            print('failed to grow')
-            sys.exit()
-        # selecting one of neigbours
-        selected = np.argmax(counts_ > np.random.rand())
-        #         print(neighbours[selected])
-        res.append(neighbours[selected][1:])
-
-
-        return regrow(*neighbours[selected], res)
-
-
-
-def regrow_biased(n, dx, dy, dz, res, w, alpha, k):
-    """
-    recursive biased regrow.
-    The generated coordinates are biased
-    """
-
-    if n == 1:
-        return res + [[0, 0, 0]], w, k
-    else:
-        neighbours = [[n - 1, dx - 1, dy, dz],
-                      [n - 1, dx + 1, dy, dz],
-                      [n - 1, dx, dy - 1, dz],
-                      [n - 1, dx, dy + 1, dz],
-                      [n - 1, dx, dy, dz - 1],
-                      [n - 1, dx, dy, dz + 1]
-                      ]
-        counts = []
-        tmp = 0
-        all_coincidence = []
-        for neighbour in neighbours:
-
-            # Lets do the following: a trial coordinate can bring 0 or 1 as an additional contribution
-            # to the number of coordinate coinsidence.
-            n_coincide = 0
-            if neighbour[1:] in res:
-                n_coincide = 1
-            all_coincidence.append(n_coincide)
-
-            # checking if outside the box
-            is_inside = aux.is_inside_box(*neighbour[1:])
+                is_inside = aux.is_inside_box(*neighbour[1:])
             # if (is_inside ==0): print("OUTSiDE")
 
 
             #             print(coords, n_coincide)
             count = consts.caches[neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3])]
             counts.append(np.exp(-alpha * n_coincide) * count * is_inside)
-            tmp += count* is_inside  # accumulating the denominator
+            tmp += count * is_inside  # accumulating the denominator
 
         # calculating W
         w = w * sum(counts) / tmp
@@ -263,31 +148,162 @@ def regrow_saw(n, dx, dy, dz, res, w, alpha, k):
 
             # if there is already such a point, set n_coincide=0 to filter out that trial
             n_coincide = 1
-            if neighbour[1:] in res:
+            if (neighbour[1:] in res) or neighbour[1:] == [0, 0, 0]:
                 n_coincide = 0
             all_coincidence.append(n_coincide)
+            # checking if outside the box
+            if consts.RUN_FREE:
+                is_inside = 1
+            else:
+                is_inside = aux.is_inside_box(*neighbour[1:])
 
             #             print(coords, n_coincide)
             count = consts.caches[neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3])]
-            counts.append(count * n_coincide)
-            tmp += count* n_coincide  # accumulating the denominator
-
+            counts.append(count * n_coincide * is_inside)
+            tmp += count * n_coincide * is_inside  # accumulating the denominator
+        if tmp == 0:
+            # print('failed to grow... restarting')
+            return res + [[0, 0, 0]], w, k
         # calculating W
-        w = w * sum(counts) / tmp
+        # w = w * sum(counts) / tmp
         #         print(w)
         # normalising p_i
         counts = [c / sum(counts) for c in counts]
 
         # making cumulative sum
         counts_ = np.cumsum(counts)
+
         if sum(counts_) == 0:
             print('failed to grow')
             sys.exit()
         # selecting one of neigbours
         selected = np.argmax(counts_ > np.random.random())
+        # if tmp ==0: print('all zeros, no saws', selected, res, neighbours[selected][1:])
         res.append(neighbours[selected][1:])
+
         if counts[selected] == 0:
             print('SELECTED NOT POSSIBLE')
-        k += all_coincidence[selected]
-        return regrow_biased(*neighbours[selected], res, w, alpha, k)
 
+        k += all_coincidence[selected]
+        return regrow_saw(*neighbours[selected], res, w, alpha, k)
+
+# """
+# Changing chain conformation
+# """
+#
+# import math
+# import  numpy as np
+# import  sys
+# try:
+#     import consts, aux
+# except:
+#     from chromosome_segregation import consts, aux
+
+# def regrow(n, dx, dy, dz, res):
+#     """
+#     recursive uniform regrow.
+#     The generated coordinates are uniform random
+#     """
+#
+#     if n == 1:
+#         return res + [[0, 0, 0]]
+#     else:
+#         neighbours = [[n - 1, dx - 1, dy, dz],
+#                       [n - 1, dx + 1, dy, dz],
+#                       [n - 1, dx, dy - 1, dz],
+#                       [n - 1, dx, dy + 1, dz],
+#                       [n - 1, dx, dy, dz - 1],
+#                       [n - 1, dx, dy, dz + 1]
+#                       ]
+#         counts = []
+#         for n_ in neighbours:
+#
+#             # checking if outside the box
+#             is_inside = aux.is_inside_box(*n_[1:])
+#
+#             if is_inside ==1:
+#                 counts.append(consts.caches[n_[0] - 1, abs(n_[1]), abs(n_[2]), abs(n_[3])])
+#             else:
+#                 counts.append(0)
+#
+#         # normalising
+#         counts = [c / sum(counts) for c in counts]
+#         # uniform sampling
+#         #         nonzeros = [v for v in counts if v>0]
+#         #         counts = [1/len(nonzeros) if c !=0 else c for c in counts]
+#         # wrong sampling
+#         #         counts = [1/6. for i in range(6)]
+#
+#         #         print(counts)
+#
+#         # making cumulative sum
+#         counts_ = np.cumsum(counts)
+#         if sum(counts_) == 0:
+#             print('failed to grow')
+#             sys.exit()
+#         # selecting one of neigbours
+#         selected = np.argmax(counts_ > np.random.rand())
+#         #         print(neighbours[selected])
+#         res.append(neighbours[selected][1:])
+#
+#
+#         return regrow(*neighbours[selected], res)
+
+
+
+# def regrow_biased(n, dx, dy, dz, res, w, alpha, k):
+#     """
+#     recursive biased regrow.
+#     The generated coordinates are biased
+#     """
+#
+#     if n == 1:
+#         return res + [[0, 0, 0]], w, k
+#     else:
+#         neighbours = [[n - 1, dx - 1, dy, dz],
+#                       [n - 1, dx + 1, dy, dz],
+#                       [n - 1, dx, dy - 1, dz],
+#                       [n - 1, dx, dy + 1, dz],
+#                       [n - 1, dx, dy, dz - 1],
+#                       [n - 1, dx, dy, dz + 1]
+#                       ]
+#         counts = []
+#         tmp = 0
+#         all_coincidence = []
+#         for neighbour in neighbours:
+#
+#             # Lets do the following: a trial coordinate can bring 0 or 1 as an additional contribution
+#             # to the number of coordinate coinsidence.
+#             n_coincide = 0
+#             if neighbour[1:] in res:
+#                 n_coincide = 1
+#             all_coincidence.append(n_coincide)
+#
+#             # checking if outside the box
+#             is_inside = aux.is_inside_box(*neighbour[1:])
+#             # if (is_inside ==0): print("OUTSiDE")
+#
+#
+#             #             print(coords, n_coincide)
+#             count = consts.caches[neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3])]
+#             counts.append(np.exp(-alpha * n_coincide) * count * is_inside)
+#             tmp += count* is_inside  # accumulating the denominator
+#
+#         # calculating W
+#         w = w * sum(counts) / tmp
+#         #         print(w)
+#         # normalising p_i
+#         counts = [c / sum(counts) for c in counts]
+#
+#         # making cumulative sum
+#         counts_ = np.cumsum(counts)
+#         if sum(counts_) == 0:
+#             print('failed to grow')
+#             sys.exit()
+#         # selecting one of neigbours
+#         selected = np.argmax(counts_ > np.random.random())
+#         res.append(neighbours[selected][1:])
+#         if counts[selected] == 0:
+#             print('SELECTED NOT POSSIBLE')
+#         k += all_coincidence[selected]
+#         return regrow_biased(*neighbours[selected], res, w, alpha, k)
