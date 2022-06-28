a7b4969e0cef7a5823dc700ce73121f5d9f34f49
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2022-06-28 11:28:35 +0200

    fix  in import

--- chromosome_segregation/run_saw.py
+++ chromosome_segregation/run_saw.py
@@ -47,7 +47,7 @@ def plot(total_results):
         y = []
         boxes = [[i, thickness, thickness] for i in range(n_boxes, 0, -1)]
         for i in range(len(boxes)):
-            n = get_n(boxes[i])
+            n = get_n(boxes[i], density)
             x.append(1 - n / get_n(boxes[0]))
             #         x.append(n)
             specific_free_energy = f(1 / n, a=0.375, b=.1347, c=-.2459)
