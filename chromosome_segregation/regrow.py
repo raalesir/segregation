78a79f83009520453d614880f3afb424217b7211
Alexey Siretskiy <alexey.siretskiy@gmail.com>
2022-08-19 10:21:26 +0200

    adding prove segregation

--- chromosome_segregation/regrow.py
+++ chromosome_segregation/regrow.py
@@ -122,6 +122,8 @@ def regrow_biased(n, dx, dy, dz, res, w, alpha, k):
         return regrow_biased(*neighbours[selected], res, w, alpha, k)
 
 
+
+
 def regrow_saw(n, dx, dy, dz, res, w, alpha, k):
     """
     recursive  regrow for SAW only.
@@ -179,6 +181,71 @@ def regrow_saw(n, dx, dy, dz, res, w, alpha, k):
 
         k += all_coincidence[selected]
         return regrow_saw(*neighbours[selected], res, w, alpha, k)
+
+
+def regrow_saw_segregation_prove(n, dx, dy, dz, res, w, alpha, k, coords):
+    """
+    recursive  regrow for SAW only.
+    """
+
+    if n == 1:
+        return res + [[0, 0, 0]], w, k
+    else:
+        neighbours = [[n - 1, dx - 1, dy, dz],
+                      [n - 1, dx + 1, dy, dz],
+                      [n - 1, dx, dy - 1, dz],
+                      [n - 1, dx, dy + 1, dz],
+                      [n - 1, dx, dy, dz - 1],
+                      [n - 1, dx, dy, dz + 1]
+                      ]
+        counts = []
+        tmp = 0
+        all_coincidence = []
+        for neighbour in neighbours:
+
+            # if there is already such a point, set n_coincide=0 to filter out that trial
+            n_coincide = 1
+            if (neighbour[1:] in res) or (neighbour[1:] == [0, 0, 0]) or (neighbour[1:] in coords):
+                n_coincide = 0
+            all_coincidence.append(n_coincide)
+            # checking if outside the box
+            is_inside =  aux.is_inside_box(*neighbour[1:])
+
+            #             print(coords, n_coincide)
+            count = consts.caches[neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3])]
+            # print(count, neighbour[0] - 1, abs(neighbour[1]), abs(neighbour[2]), abs(neighbour[3]))
+
+            counts.append(count * n_coincide * is_inside)
+            tmp += count * n_coincide * is_inside  # accumulating the denominator
+        if sum(counts) == 0:
+            # print('failed to grow... restarting')
+            return res + [[0, 0, 0]], w, k
+        # calculating W
+        # w = w * sum(counts) / tmp
+        #         print(w)
+
+        # normalising p_i
+        # counts = [c / sum(counts) for c in counts]
+
+        # making cumulative sum
+        counts_ = np.cumsum( [c / sum(counts) for c in counts] )
+
+        if sum(counts_) == 0:
+            print('failed to grow')
+            sys.exit()
+        # selecting one of neighbours
+        selected = np.argmax(counts_ > np.random.random())
+        # if tmp ==0: print('all zeros, no saws', selected, res, neighbours[selected][1:])
+        res.append(neighbours[selected][1:])
+
+        # calculating the probability of the configuration
+        w = w * counts[selected]/tmp
+
+        if counts[selected] == 0:
+            print('SELECTED NOT POSSIBLE')
+
+        k += all_coincidence[selected]
+        return regrow_saw_segregation_prove(*neighbours[selected], res, w, alpha, k, coords)
 
 # """
 # Changing chain conformation
